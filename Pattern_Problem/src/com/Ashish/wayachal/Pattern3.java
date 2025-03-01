1ed054f1c9bad6567bd6e15bbed4220894826ae7
Ashish <ashishwayachal14@gmail.com>
2025-03-01 11:29:13 +0530

    Pattern Problem...

--- Pattern_Problem/src/com/Ashish/wayachal/Pattern3.java
+++ Pattern_Problem/src/com/Ashish/wayachal/Pattern3.java
@@ -2,8 +2,48 @@ package com.Ashish.wayachal;
 
 public class Pattern3 {
     public static void main(String[] args) {
-        pattern6(5);
+        pattern8(500);
 
+    }
+
+    static void pattern8(int n)
+    {
+        for (int row = 1; row <2*n ; row++) {
+            int c=row >n ? 2*n -row:row;
+            for (int space = 0; space <n-c ; space++) {
+                System.out.print("  ");
+
+            }
+            for (int col = c; col >=1 ; col--) {
+                System.out.print(col+" ");
+
+            } for (int col = 2; col <=c ; col++) {
+                System.out.print(col+" ");
+
+            }
+            System.out.println();
+
+        }
+    }
+
+    static void pattern7(int n)
+    {
+        for (int row = 0; row <n ; row++) {
+
+            for (int space = 0; space <n-row ; space++) {
+                System.out.print("  ");
+
+            }
+            for (int col = row; col >=1 ; col--) {
+                System.out.print(col+" ");
+
+            } for (int col = 2; col <=row ; col++) {
+                System.out.print(col+" ");
+
+            }
+            System.out.println();
+            
+        }
     }
     static void pattern6(int n)
     {
