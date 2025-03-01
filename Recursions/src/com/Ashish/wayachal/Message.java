50cb8030624defca73eebc3a14701ba28b2510b3
Ashish <ashishwayachal14@gmail.com>
2025-03-01 17:29:22 +0530

    Recursions...

---
+++ Recursions/src/com/Ashish/wayachal/Message.java
@@ -0,0 +1,32 @@
+package com.Ashish.wayachal;
+
+public class Message {
+    public static void main(String[] args) {
+message();
+    }
+ static void message()
+ {
+     System.out.println("Hello");
+     message2();
+ }static void message2()
+
+ {
+     System.out.println("Hello");
+     message3();
+ }static void message3()
+ {
+     System.out.println("Hello");
+     message4();
+ }static void message4()
+ {
+     System.out.println("Hello");
+     message5();
+ }static void message5()
+ {
+     System.out.println("Hello");
+     message6();
+ }static void message6()
+ {
+     System.out.println("Hello");
+ }
+}
